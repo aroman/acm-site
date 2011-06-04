7550d85d42687b78af23a5337ae511eff7ad9fd5
Vincent Siao <vincentsiao@gmail.com>
2011-06-03 23:31:53 -0700

    we page jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,8 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function() {
+
+  // login box
   $("#l-open").click(function() {
     $("#l-open").slideUp(200);
     $("#auth").slideDown();
@@ -12,4 +14,30 @@ $(document).ready(function() {
     $("#auth").slideUp();
     $("#l-open").delay(500).fadeIn(200);
   });
+
+  // we page
+  $("#link-jobs").click(function() {
+    $("#we-menu").slideUp(200);
+    $("#we-jobs").delay(300).slideDown();
+  });
+  $("#link-code").click(function() {
+    $("#we-menu").slideUp(200);
+    $("#we-code").delay(300).slideDown();
+  });
+  $("#link-projects").click(function() {
+    $("#we-menu").slideUp(200);
+    $("#we-projects").delay(300).slideDown();
+  });
+  $("#link-journals").click(function() {
+    $("#we-menu").slideUp(200);
+    $("#we-journals").delay(300).slideDown();
+  });
+  $(".we-back").click(function() {
+    $("#we-jobs").slideUp(200);
+    $("#we-code").slideUp(200);
+    $("#we-projects").slideUp(200);
+    $("#we-journals").slideUp(200);
+    $("#we-menu").delay(300).slideDown(200);
+  });
+
 });
