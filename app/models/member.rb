c15391a28969931b7743606352298d87b57085f9
Adam Weis <aweis@aweis-wsl.internal.salesforce.com>
2011-07-12 23:44:16 -0700

    changes to member to make accessible variables

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ActiveRecord::Base
   # new columns need to be added here to be writable through mass assignment
-  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation, :first_namei, :last_name, :andrew_id, :major, :officer_position, :committe, :phone, :shirt_size, :year, :role
 
   ROLES = [['Administrator', :admin],['User', :user]]
 
