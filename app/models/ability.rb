2426d883b1a177f446aff6c7f61e3e8c2bc633a1
Adam Weis <aweis@aweis-wsl.internal.salesforce.com>
2011-07-13 00:31:24 -0700

    ui changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,10 @@ class Ability
     
     if member.role? :admin
       can :manage, :all
+#    elsif member.role? :user  #this is so a user can edit own details
+ #     can :manage, Member do |memberi|
+#        memberi.try == member
+ #     end
     else
       can :read, :all
     end
